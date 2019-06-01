eb8441ab46b84be302e32a8b4b4d0738880cfcb7
CiSigEp <cisigep@gmail.com>
2019-06-01 13:41:54 -0500

    Search button generate, needs special check

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,30 @@
 $(function (){
+
+    // Add Search click event
+    $("#searchButton").click(function() {
+        var queryValue = $("#query").val();
+
+        if(queryValue.length === 0)
+            return;
+
+        // Create a button for the query and attach it if it doesn't already exist
+        if(!$("button[data-query="+ queryValue + "]").length){
+            var newBtn = $("<button>");
+            newBtn.attr("data-query", queryValue);
+            newBtn.addClass("btn btn-dark mr-1 mb-1");
+            newBtn.text(queryValue);
+            $("#buttonContainer").append(newBtn);
+        }
+
+
+        // Send a request for the query
+
+
+
+    });
+
+    // Add click events for the buttons
+
     /* $.get({
         url : 'https://api.giphy.com/v1/gifs/search',
         data : {
