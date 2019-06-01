2470deaa7c905eaad6e20ba42bd9fc9aea4179e5
CiSigEp <cisigep@gmail.com>
2019-06-01 13:55:00 -0500

    Add AJAX Requests

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,23 @@
 $(function (){
 
+    // Function to send a request for the gifs
+    function sendRequest(query) {
+        $.get({
+            url : 'https://api.giphy.com/v1/gifs/search',
+            data : {
+                api_key: 'YOUR_KEY',
+                q : query,
+                limit : 10,
+                offset : 0,
+                rating : 'pg'
+            }
+        }).done(function(data){
+            console.log(data);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.log("FAILED: " + errorThrown);
+        });
+    }
+
     // Add Search click event
     $("#searchButton").click(function() {
         var queryValue = $("#query").val();
@@ -11,33 +29,21 @@ $(function (){
         if(!$("button[data-query="+ queryValue + "]").length){
             var newBtn = $("<button>");
             newBtn.attr("data-query", queryValue);
-            newBtn.addClass("btn btn-dark mr-1 mb-1");
+            newBtn.addClass("query-button btn btn-dark mr-1 mb-1");
             newBtn.text(queryValue);
             $("#buttonContainer").append(newBtn);
         }
 
 
         // Send a request for the query
+        sendRequest(queryValue);
 
-
+        $("#query").val("");
 
     });
 
     // Add click events for the buttons
-
-    /* $.get({
-        url : 'https://api.giphy.com/v1/gifs/search',
-        data : {
-            api_key: 'KEY_HERE',
-            q : 'VALUE_HERE',
-            limit : 10,
-            offset : 0,
-            rating : 'pg'
-        }
-    }).done(function(data){
-        console.log(data);
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        console.log("FAILED: " + errorThrown);
-    }); */
-
+    $(document).on("click", ".query-button", function(){
+        sendRequest($(this).attr("data-query"));
+    });
 });
