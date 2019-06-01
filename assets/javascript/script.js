aa29f59a637290517acdf3663e0d894d8d1080ac
CiSigEp <cisigep@gmail.com>
2019-06-01 13:16:12 -0500

    Structure, colors to indicate where each thing is

---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+$(function (){
+    /* $.get({
+        url : 'https://api.giphy.com/v1/gifs/search',
+        data : {
+            api_key: 'KEY_HERE',
+            q : 'VALUE_HERE',
+            limit : 10,
+            offset : 0,
+            rating : 'pg'
+        }
+    }).done(function(data){
+        console.log(data);
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("FAILED: " + errorThrown);
+    }); */
+
+});
