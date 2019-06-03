0b70503ba7b3276a1360c53cc7a8fb32a3d77a80
CiSigEp <cisigep@gmail.com>
2019-06-03 13:28:51 -0500

    Add load more button, topics

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,30 +1,50 @@
 $(function (){
 
+    var numGifs = 0;
+    var currentQuery = "";
+    // Our topic is "Vehicles"
+    var topics = ["Car", "Truck", "Bus", "Motorcycle", "Bicycle", "Scooter", "Tractor", "ATV", "Chariot", "Carriage", "Wagon", "Train", "Plane", "Helicopter", "Boat", "Blimp", "Hot-Air Balloon", "Sled", "Snowmobile", "Rocket", "Spacecraft"];
+
+    function renderButtons(topics) {
+        $("#buttonContainer").empty();
+        topics.forEach(function(topic){
+            var newBtn = $("<button>");
+            newBtn.attr("data-query", topic);
+            newBtn.addClass("query-button btn btn-dark mr-1 mb-1");
+            newBtn.text(topic);
+            $("#buttonContainer").append(newBtn);
+        });
+    }
+
     // Function to send a request for the gifs
-    function sendRequest(query, resultLimit, page) {
-        if(!resultLimit)
-            resultLimit = 10;
-        if(!page)
-            page = 0;
+    function sendRequest(query) {
+        currentQuery = query;
         $.get({
             url : 'https://api.giphy.com/v1/gifs/search',
             data : {
-                api_key: 'YOUR_KEY',
+                api_key: 'PdaxPTr3o2UH5Fv2shcFiOFMo8Rrqggr',
                 q : query,
-                limit : resultLimit,
-                offset : resultLimit * page,
+                limit : 10,
+                offset : numGifs,
                 rating : 'pg'
             }
         }).done(function(data){
             var imageData = data.data;
             // Clear the container
-            $("#gifContainer").empty();
+            if(numGifs === 0)
+                $(".gif-row").remove();
 
             var row;
+            var grabbedRow = false;
+            if(numGifs % 4 !== 0) {
+                row = $(".gif-row:last");
+                grabbedRow = true;
+            }
             for(var i = 0; i < imageData.length; i++){
-                if(i % 4 === 0) { 
+                if(numGifs % 4 === 0) { 
+                    grabbedRow = false;
                     row = $("<div>");
-                    row.addClass("row");
+                    row.addClass("row gif-row");
                 }
 
                 // Get all the necessary data
@@ -37,7 +57,8 @@ $(function (){
                 img.attr("data-state", "still");
                 img.attr("data-still-url", still);
                 img.attr("src", still);
-                img.attr("data-animated-url", animated); 
+                img.attr("data-animated-url", animated);
+                img.attr("alt", query + " gif " + (numGifs + 1)); 
                 img.addClass("gif-image img-fluid");
                 
                 // Create our rating
@@ -46,7 +67,7 @@ $(function (){
 
                 // Create our column
                 var col = $("<div>");
-                col.addClass("col-md-3 col-12");
+                col.addClass("col-lg-3 col-md-6 col-12");
                 
                 col.append(p);
                 col.append(img);
@@ -54,9 +75,15 @@ $(function (){
                 row.append(col);
 
                 // Append our row if we run out of images or we're at the end of our row.
-                if(i % 4 === 3 || i === imageData.length - 1)
-                    $("#gifContainer").append(row);
+                if((numGifs % 4 === 3 && !grabbedRow) || i === imageData.length - 1)
+                    $("#loadRow").before(row);
+
+                numGifs++;
+
+
             }
+
+            $("#loadRow").removeClass("d-none");
         }).fail(function(jqXHR, textStatus, errorThrown) {
             console.log("FAILED: " + errorThrown);
         });
@@ -72,13 +99,12 @@ $(function (){
             return;
 
         // Create a button for the query and attach it if it doesn't already exist
-        if(!$("button[data-query="+ queryValue + "]").length){
-            var newBtn = $("<button>");
-            newBtn.attr("data-query", queryValue);
-            newBtn.addClass("query-button btn btn-dark mr-1 mb-1");
-            newBtn.text(queryValue);
-            $("#buttonContainer").append(newBtn);
+        if(topics.map(function(item){ return item.toLowerCase(); }).indexOf(queryValue.toLowerCase()) < 0){
+            topics.push(queryValue);
+            renderButtons(topics);
         }
+
+        numGifs = 0;
 
         // Send a request for the query
         sendRequest(queryValue);
@@ -88,8 +114,14 @@ $(function (){
 
     });
 
+    // Add Click event for the load button.
+    $("#loadButton").click(function(){
+        sendRequest(currentQuery);
+    });
+
     // Add click events for the buttons
     $(document).on("click", ".query-button", function(){
+        numGifs = 0;
         sendRequest($(this).attr("data-query"));
     });
 
@@ -109,4 +141,12 @@ $(function (){
             element.attr("data-state", "still")
         }
     });
+
+    $(".query-input").keyup(function(e){
+        if(e.which === 13)
+            $(".search-button[data-which=" + $(this).attr("data-which") + "]").click();
+        return false;
+    })
+
+    renderButtons(topics);
 });
